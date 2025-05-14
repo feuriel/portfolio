1b9b752cad169c9894f3dfc5bf7f17a89f118ffb
GabrielS <gabriel.somogyi.feuga@gmail.com>
2025-05-14 21:56:09 +0200

    media section in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,10 +3,10 @@ import { Media } from "../Media/Media.jsx";
 
 export const Footer = () => {
   return (
-    <section id="footer" className="flex justify-center items-center">
+    <section id="footer" className="flex justify-center items-center pb-12">
       <section className="w-full about max-w-7xl">
         <div>Let's connect</div>
-        <Media />
+        <Media withGithub={true} withGoogle={true} withLinkedin={true} />
       </section>
     </section>
   );
--- src/components/Media/Media.jsx
+++ src/components/Media/Media.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { Linkedin } from "./Linkedin";
+import { GMail } from "./GMail";
+import "./Media.css";
+import { Github } from "./Github";
 
-export const Media = () => {
+export const Media = ({
+  withLinkedin = false,
+  withGithub = false,
+  withGoogle = false,
+}) => {
   return (
-    <div>
-      Media
-      <Linkedin />
+    <div className="media flex justify-center gap-4">
+      {withLinkedin && <Linkedin />}
+      {withGithub && <Github />}
+      {withGoogle && <GMail />}
     </div>
   );
 };
