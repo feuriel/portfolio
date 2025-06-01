6d98921a564b29d3b37273f97412fc56a2144415
GabrielS <gabriel.somogyi.feuga@gmail.com>
2025-06-01 21:59:00 +0200

    updating projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Section } from "../Section/Section";
 import "./Projects.css";
 import { ProjectIllustration } from "./ProjectIllustration";
 import { ProjectDetails } from "./ProjectDetails";
 
 export const Projects = () => {
+  let [isUnlocked, setIsUnlocked] = useState(false);
   return (
     <Section
       id="projects"
@@ -13,8 +14,8 @@ export const Projects = () => {
       finished={true}
     >
       <h3 className="mt-[-2rem] mb-[2rem] md:hidden">Selected projects :</h3>
-      <div className="grid grid-cols-2 xl:grid-cols-3 gap-6 grid-rows-[minmax(200px,1fr)]">
-        <div className="row-start-1 col-start-1">
+      <div className="pb-[2rem] flex flex-col sm:grid sm:grid-cols-2 xl:grid-cols-3 gap-6 grid-rows-[minmax(200px,1fr)]">
+        <div className="row-start-1 col-start-1 hidden sm:block">
           <ProjectDetails
             title="CXC"
             role="Manager, UX/UI Design, Development"
@@ -26,20 +27,20 @@ export const Projects = () => {
             </p>
           </ProjectDetails>
         </div>
-        <div className="row-start-1 col-start-2 ml-[-1rem] ps-[1rem]">
+        <div className="row-start-1 col-start-2 xl:ml-[-1rem] lg:ps-[1rem]">
           <ProjectIllustration i={0} />
         </div>
-        <div className="row-start-2 col-start-2">
+        <div className="row-start-2 col-start-2 hidden sm:block">
           <ProjectDetails
             title="Circles of Lines"
             role="Development"
             year="2025"
           ></ProjectDetails>
         </div>
-        <div className="row-start-2 col-start-1">
+        <div className="sm:row-start-2 sm:col-start-1">
           <ProjectIllustration i={1} />
         </div>
-        <div className="row-start-3 col-start-1 xl:col-start-3">
+        <div className="row-start-3 col-start-1 xl:col-start-3 hidden sm:block">
           <ProjectDetails
             title="Chateau de Morin"
             role="Development"
@@ -49,9 +50,9 @@ export const Projects = () => {
         <div className="row-start-3 col-start-2">
           <ProjectIllustration i={2} />
         </div>
-        <div className="row-start-4 col-start-2">
+        <div className="row-start-4 col-start-2 hidden sm:block">
           <ProjectDetails
-            title="Under Construction"
+            title="Coming Soon"
             role="Development"
             year="2025"
           ></ProjectDetails>
@@ -59,7 +60,7 @@ export const Projects = () => {
         <div className="row-start-4 col-start-1 xl:col-start-3">
           <ProjectIllustration i={3} />
         </div>
-        <div className="row-start-5 col-start-1">
+        <div className="row-start-5 col-start-1 hidden sm:block">
           <ProjectDetails
             title="Math is Art"
             role="Development"
@@ -69,7 +70,7 @@ export const Projects = () => {
         <div className="row-start-5 col-start-2">
           <ProjectIllustration i={4} />
         </div>
-        <div className="row-start-6 col-start-2">
+        <div className="row-start-6 col-start-2 hidden sm:block">
           <ProjectDetails
             title="Hawaya"
             role="Direction of Engineering"
@@ -80,16 +81,20 @@ export const Projects = () => {
           <ProjectIllustration i={5} />
         </div>
       </div>
-      <div className="text-sm">And some older projects...</div>
+      <div className="text-sm pb-[1rem]">And some [fun] projects... 💡</div>
       <div className="text-sm grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-4">
-        <div>random1</div>
-        <div>random2</div>
-        <div>random3</div>
-        <div>random4</div>
-        <div>random5</div>
-        <div>random6</div>
-        <div>random7</div>
-        <div>random8</div>
+        {/* <RandomProject name="Memo-riel" icon="🔒" /> */}
+        <div>🔒 Memo-riel</div>
+        <div>🔒 CSS Anims</div>
+        <div>🔒 CSS Clock</div>
+        <div>🔒 Pixel Art MonaLisa</div>
+        <div>🔒 Phaser Star</div>
+        <div>🔒 Outer Circle</div>
+        <div>🔒 Bulbhead</div>
+        <div>🔒 Landing Page</div>
+        <div>🔒 Loadinmage</div>
+        <div>🔒 Locked</div>
+        <div>🔒 Inner Circle</div>
       </div>
     </Section>
   );
