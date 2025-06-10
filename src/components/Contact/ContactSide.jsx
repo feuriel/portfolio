35c3e5bd6ea8e0d2c3c3a4d768e517d837b78293
GabrielS <gabriel.somogyi.feuga@gmail.com>
2025-06-10 21:45:54 +0200

    adding resume link

--- src/components/Contact/ContactSide.jsx
+++ src/components/Contact/ContactSide.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Media } from "../Media/Media.jsx";
 import { AnimatedDiv } from "../../utility/AnimatedDiv.jsx";
+import Resume from "./Gabriel Somogyi - Resume.pdf";
 
 export const ContactSide = ({ className }) => {
   return (
@@ -8,11 +9,17 @@ export const ContactSide = ({ className }) => {
       <h3>Let's connect !</h3>
       <p>
         Feel free to reach out to me directly via email at{" "}
-        <a href="mailto:gabriel.somogyi.feuga@gmail.com">
+        <a href="mailto:gabriel.somogyi.feuga@gmail.com" className="underline">
           gabriel.somogyi.feuga@gmail.com
         </a>
       </p>
-      <p>You can also reach on my social network.</p>
+      <p>
+        You can also reach out on my social network and{" "}
+        <a href={Resume} download className="underline">
+          download my resume
+        </a>
+        .
+      </p>
       <Media
         withLinkedin={true}
         withGithub={true}
